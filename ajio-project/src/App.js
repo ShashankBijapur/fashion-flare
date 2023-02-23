508ef514c658f2574ebc36d81143fcc4e5f9d22f
shagunsharma6677 <shagunsharma6677@gmail.com>
2023-02-23 15:00:49 +0530

    folder update

--- ajio-project/src/App.js
+++ ajio-project/src/App.js
@@ -1,12 +1,24 @@
 
 import './App.css';
-import Home from './Components/Home/Home';
+import AdminPage from './component/Admin/Admin';
+import Home from './component/Home/Home';
+import Kids from './component/Kids/Kids';
+import Mens from './component/Mens/Mens';
+
+import Womens from './component/Womens/Womens';
+
 
 
 function App() {
   return (
     <div className="App">
-     <Home />
+      {/* <Home/> */}
+      {/* <AdminPage/> */}
+      {/* <Mens/> */}
+      {/* <Womens/> */}
+      <Kids/>
+      
+    
     </div>
   );
 }
