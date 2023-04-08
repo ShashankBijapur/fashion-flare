b68a617cb7920aba6e7c0fe8713fbcfbfbdbe81d
morevishal99 <morevishal060298@gmail.com>
2023-04-08 18:44:39 +0530

    added responsiveness to womens , singleproduct page ,navbar and footer  fixed  ui fixed on rendering

--- ajio-project/src/component/Womens/SingleCardPage.jsx
+++ ajio-project/src/component/Womens/SingleCardPage.jsx
@@ -1,28 +1,22 @@
 import React, { useState, useEffect } from 'react'
-import {
-    Text,
-} from '@chakra-ui/react'
-import { Box, Button, Drawer, DrawerBody, DrawerContent, DrawerHeader, DrawerOverlay, Fade, Radio, RadioGroup, ScaleFade, Slide, SlideFade, Stack, useDisclosure } from '@chakra-ui/react'
+import { Fade, Image, Text, } from '@chakra-ui/react'
+import { Box, Button, useDisclosure } from '@chakra-ui/react'
 import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import Carousel from './Caurosal'
-import { postWishlist } from '../../redux/action'
 import { useToast } from '@chakra-ui/react'
 const SingleCardPage = () => {
     var product = JSON.parse(localStorage.getItem('ProductsDetails'))
-    const dispatch = useDispatch()
+    console.log('product: ', product);
+
     const toast = useToast()
     const { isOpen, onToggle } = useDisclosure()
     const [size, setSize] = useState('');
     const [text, setText] = useState(true)
     const navigate = useNavigate()
     const [data, setData] = React.useState([])
-    const [count, setcount] = useState(0)
-    const getsimilar = () => {
-        axios.get("https://magnificent-bass-suit.cyclic.app/similar")
-            .then(r => setData(r.data))
-    }
+    let [count, setcount] = useState(0)
     const getdata = () => {
         axios.get("https://magnificent-bass-suit.cyclic.app/similar")
             .then(r => setData(r.data))
@@ -30,7 +24,7 @@ const SingleCardPage = () => {
 
     useEffect(() => {
         getdata()
-        getsimilar()
+
     }, []);
 
     const handleSizeChange = (event) => {
@@ -39,54 +33,44 @@ const SingleCardPage = () => {
     const handletext = () => {
         setText(!text)
         if (text === true) {
-            // alert("Item added to wishlist")
             axios.post("https://magnificent-bass-suit.cyclic.app/wishlist", product)
-                .then((r) => {
-                    // console.log(r.data)
-                    dispatch(postWishlist(r.data))
-                    toast({
-                        title: `Added to Wishlist`,
-                        position: "top",
-                        status: 'success',
-                        duration: 3000,
-                        isClosable: true,
-                    })
 
-                })
+            toast({
+                title: `Product Added to Wishlist Successfully`,
+                position: "top",
+                status: 'success',
+                duration: 3000,
+                isClosable: true,
+            })
         }
-        // else if (text === false) {
-        //     alert("Item removed from wishlist")
-        // }
     }
 
     const addtobag = () => {
-        setcount(count + 1)
+        setcount(count++)
         axios.post("https://magnificent-bass-suit.cyclic.app/cart", product)
-            .then((r) => {
-                // console.log(r.data)
-                toast({
-                    title: `Added to cart`,
-                    position: "top",
-                    status: 'success',
-                    duration: 2000,
-                    isClosable: true,
-                })
-
-            })
+        toast({
+            title: `Product Added to Cart Successfully`,
+            position: "top",
+            status: 'success',
+            duration: 2000,
+            isClosable: true,
+        })
     }
+
     const navigateto = () => {
         navigate("/womens")
     }
+
     return (<>
 
 
-        <div style={{ display: "flex", justifyContent: "space-evenly", width: "90%", margin: "auto" }}>
-            <div >
-                <div style={{ backgroundColor: "rgb(235,237,236)", background: "rgb(235,237,236)" }}>
-                    <img style={{ padding: "40px", }} width={400} src={product.src} alt={product.brand} />
+        <Box display={{base:"grid",md:"flex" ,lg: "flex",}} justifyContent= "space-evenly" width= "90%" margin= "auto" >
 
-                </div>
-                <div style={{ textAlign: "left", }} >
+            <div >
+                <div style={{ placeItems:"center" }}>
+                    <Image marginLeft={"60px"} placeItems={"center"} style={{ padding: "30px", }} width={{base:"250px",md:"350px",lg:"400px"}} src={product.src} alt={product.brand} />
+
+                <div style={{ textAlign: "center", }} >
                     <Button color={"grey"} border="1px solid grey" mt={4} padding="5px" onClick={onToggle}>Returns Details</Button>
                     <Fade in={isOpen}>
                         <Box
@@ -103,6 +87,7 @@ const SingleCardPage = () => {
                         </Box>
                     </Fade>
 
+                </div>
                 </div>
             </div>
 
@@ -134,9 +119,9 @@ const SingleCardPage = () => {
                 {/* <DrawerExample/> */}
                 <div style={{ display: "grid ", gap: "10px", justifyContent: "center" }}>
                     <Text bg={"rgb(253,248,235)"} width={300} fontSize="10px" padding={"5px"} margin="auto">Select your size to know your estimated delivery date.</Text>
-                    <Button disable={count === 1} onClick={addtobag} bg={"rgb(213,162,73)"} width={300} padding={"5px"} margin="auto"> Add to Bag</Button>
+                    <Button disabled={count <= 1} onClick={addtobag} bg={"rgb(213,162,73)"} width={300} padding={"5px"} margin="auto"> Add to Bag</Button>
                     <Text width={300} fontSize="10px" padding={"5px"} margin="auto" color={"grey"}>HANDPICKED STYLES | ASSURED QUALITY</Text>
-                    <Button onClick={handletext} bg={"rgb(213,162,73)"} width={300} padding={"5px"} margin="auto"> {text ? "Save To WishList" : "Remove From WishLIst"}</Button>
+                    <Button bg={text ? "rgb(213,162,73)" : "red.600"} onClick={handletext} width={300} padding={"5px"} margin="auto"> {text ? "Save To WishList" : "Added To WishList"}</Button>
 
                 </div>
 
@@ -154,13 +139,15 @@ const SingleCardPage = () => {
 
                 </div>
             </div>
-        </div>
+        </Box>
+
 
         <div style={{ width: "80%", margin: "auto", }}>
 
-            <div >
-                <div>
 
+            <div >
+
+                <div>
                     <h1 style={{
                         fontFamily: "Lora", fontSize: "26px", fontWeight: '700', lineHeight: "1.4px", color: "rgb(88, 88, 88)", marginTop: "100px",
                     }}>About {product.brand}</h1>
@@ -173,9 +160,8 @@ const SingleCardPage = () => {
                     </div>
                     <p style={{ marginTop: "100px", width: "80%", margin: "auto", fontFamily: "areal", fontSize: "16px", color: "grey", padding: "30px" }}>Every day is different, so should be your look! Avaasa brings a designer collection of women’s fusion wear and ethnic clothes to AJIO, including floral print kurtas, block print kurtis, colourful shrugs, churidar leggings and more in a range of hues.</p>
                 </div>
-
-
             </div>
+
 
             <div>
 
@@ -186,30 +172,31 @@ const SingleCardPage = () => {
                 <div style={{ margin: "30px 0px", padding: "10px" }}>
                     <Carousel data={data} />
                 </div>
-                <div style={{ display: "flex", justifyContent: "space-between", width: "60%", margin: "auto", marginTop: "50px" }}>
+                <Box display={{base:"grid",md:"flex"}} style={{  justifyContent: "space-between", width: "60%", margin: "auto", marginTop: "50px" }}>
                     <div onClick={navigateto} style={{ padding: "30px", background: "rgb(248,248,248)" }}> <h1>View more</h1></div>
                     <div style={{ padding: "30px", background: "rgb(248,248,248)" }}> <h1>Style:view more</h1></div>
                     <div style={{ padding: "30px", background: "rgb(248,248,248)" }}> <h1>Brand:{product.brand}</h1></div>
-                </div>
+                </Box>
             </div>
+
         </div>
 
 
 
         <div style={{ display: "flex", justifyContent: "space-around", backgroundColor: "rgb(250,250,250)", marginTop: "100px" }}>
             <div style={{}}>
-                <img width="60px" src="https://cdn-icons-png.flaticon.com/512/182/182308.png" alt="" />
-                <h2>Easy Returns</h2>
-            </div>
-            <div style={{}}>
-                <img width="60px" src="https://thumbs.dreamstime.com/b/empathy-vector-icon-black-silhouette-flat-illustration-isolated-white-background-204899514.jpg" alt="" />
-                <h2>!100% Hand Picked</h2>
-            </div>
-            <div style={{}}>
-                <img width="60px" src="https://d1pt6w2mt2xqso.cloudfront.net/AcuCustom/Sitename/DAM/044/FSEweek-icon-tick.png" alt="" />
-                <h2> Assured Quality</h2>
-            </div>
-        </div>
+                 <img width="60px" src="https://cdn-icons-png.flaticon.com/512/182/182308.png" alt="" />
+                 <h2>Easy Returns</h2>
+             </div>
+             <div style={{}}>
+                 <img width="60px" src="https://thumbs.dreamstime.com/b/empathy-vector-icon-black-silhouette-flat-illustration-isolated-white-background-204899514.jpg" alt="" />
+                 <h2>!100% Hand Picked</h2>
+             </div>
+             <div style={{}}>
+                 <img width="60px" src="https://d1pt6w2mt2xqso.cloudfront.net/AcuCustom/Sitename/DAM/044/FSEweek-icon-tick.png" alt="" />
+                 <h2> Assured Quality</h2>
+             </div>
+         </div>
 
 
 
